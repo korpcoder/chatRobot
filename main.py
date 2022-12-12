f7b958779f4936521ba8c10c1c4ea7ebec95968d
korp <akai_sola@qq.com>
2022-12-12 09:08:53 +0800

    Changes

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    texts = request.json['texts']
+    # 下面是处理 texts 的代码
+    # ...
+    return jsonify({'result': '处理后的结果'})
+
+
+if __name__ == '__main__':
+    app.run()
